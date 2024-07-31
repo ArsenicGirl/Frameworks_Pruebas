5ceb1e524a8701ccc2db6b002090618db4759d40
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-07-30 22:34:44 -0500

    Consumo de API

--- SvelteP/src/routes/Pag2.svelte
+++ SvelteP/src/routes/Pag2.svelte
@@ -2,7 +2,14 @@
     import Header from '../components/Header.svelte';
 
     import { link } from 'svelte-spa-router';
-  </script>
+ 
+    let listdata = [];
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response)=>response.json())
+    .then((results)=>listdata=results)
+ 
+ </script>
   
   <main>
     <Header framework="Svelte" />
@@ -10,3 +17,9 @@
     <a href="/" use:link>Primera pagina</a>
 
   </main>
+
+  <ul>
+    {#each  listdata as values }
+        <li>{values.id}. {values.name}</li>
+    {/each}
+  </ul>
